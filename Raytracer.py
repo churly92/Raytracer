f0b4984ef85487c852cb177c405b859f6ebe5f35
churly92 <churly92@gmail.com>
2021-09-27 21:25:56 -0600

    Planes and AABBs

--- Raytracer.py
+++ Raytracer.py
@@ -3,11 +3,13 @@ from obj import *
 from figures import *
 
 # Dimensiones
-width = 1024
+width = 512
 height = 512
 
 # Materiales
+wood = Material(diffuse = (0.6,0.2,0.2), spec = 64)
 stone = Material(diffuse = (0.4,0.4,0.4), spec = 64)
+
 gold = Material(diffuse = (1, 0.8, 0 ),spec = 32, matType = REFLECTIVE)
 mirror = Material(spec = 128, matType = REFLECTIVE)
 
@@ -26,10 +28,14 @@ rtx.dirLight = DirectionalLight(direction = V3(1, -1, -2), intensity = 0.5)
 rtx.pointLights.append( PointLight(position = V3(0, 2, 0), intensity = 0.5))
 
 # Objetos
-rtx.scene.append( Sphere(V3(-6,0,-8), 1.5, mirror ))
-rtx.scene.append( Sphere(V3(-2,0,-8), 1.5, water ))
-rtx.scene.append( Sphere(V3(2,0,-8), 1.5, glass ))
-rtx.scene.append( Sphere(V3(6,0,-8), 1.5, diamond ))
+rtx.scene.append( Sphere(V3(-2,2,-8), 1, mirror) )
+rtx.scene.append( Sphere(V3(2,2,-8), 1, glass) )
+
+rtx.scene.append( AABB(V3(-2,-2,-8), V3(2,2,2), mirror) )
+rtx.scene.append( AABB(V3(2,-2,-8), V3(2,2,2), glass) )
+
+
+
 
 
 # Terminar
--- figures.py
+++ figures.py
@@ -83,6 +83,88 @@ class Sphere(object):
                           sceneObject = self)
 
 
+class Plane(object):
+    def __init__(self, position, normal, material = Material()):
+        self.position = position
+        self.normal = normal / np.linalg.norm(normal)
+        self.material = material
+
+    def ray_intersect(self, orig, dir):
+        #t = (( planePos - origRayo) dot planeNormal) / (dirRayo dot planeNormal)
+        denom = np.dot(dir, self.normal)
+
+        if abs(denom) > 0.0001:
+            num = np.dot(np.subtract(self.position, orig), self.normal)
+            t = num / denom
+            if t > 0:
+                # P = O + t * D
+                hit = np.add(orig, t * np.array(dir))
+
+                return Intersect(distance = t,
+                                 point = hit,
+                                 normal = self.normal,
+                                 sceneObject = self)
+
+        return None
+
+class AABB(object):
+    # Axis Aligned Bounding Box
+    def __init__(self, position, size, material = Material()):
+        self.position = position
+        self.size = size
+        self.material = material
+        self.planes = []
+
+        self.boundsMin = [0,0,0]
+        self.boundsMax = [0,0,0]
+
+        halfSizeX = size[0] / 2
+        halfSizeY = size[1] / 2
+        halfSizeZ = size[2] / 2
+
+        #Sides
+        self.planes.append(Plane( np.add(position, V3(halfSizeX,0,0)), V3(1,0,0), material))
+        self.planes.append(Plane( np.add(position, V3(-halfSizeX,0,0)), V3(-1,0,0), material))
+
+        # Up and down
+        self.planes.append(Plane( np.add(position, V3(0,halfSizeY,0)), V3(0,1,0), material))
+        self.planes.append(Plane( np.add(position, V3(0,-halfSizeY,0)), V3(0,-1,0), material))
+
+        # Front and Back
+        self.planes.append(Plane( np.add(position, V3(0,0,halfSizeZ)), V3(0,0,1), material))
+        self.planes.append(Plane( np.add(position, V3(0,0,-halfSizeZ)), V3(0,0,-1), material))
+
+        #Bounds
+        epsilon = 0.001
+        for i in range(3):
+            self.boundsMin[i] = self.position[i] - (epsilon + self.size[i]/2)
+            self.boundsMax[i] = self.position[i] + (epsilon + self.size[i]/2)
+
+
+    def ray_intersect(self, orig, dir):
+        intersect = None
+        t = float('inf')
+
+        for plane in self.planes:
+            planeInter = plane.ray_intersect(orig, dir)
+            if planeInter is not None:
+                # Si estoy dentro de los bounds
+                if planeInter.point[0] >= self.boundsMin[0] and planeInter.point[0] <= self.boundsMax[0]:
+                    if planeInter.point[1] >= self.boundsMin[1] and planeInter.point[1] <= self.boundsMax[1]:
+                        if planeInter.point[2] >= self.boundsMin[2] and planeInter.point[2] <= self.boundsMax[2]:
+                            #Si soy el plano mas cercano
+                            if planeInter.distance < t:
+                                t = planeInter.distance
+                                intersect = planeInter
+
+
+        if intersect is None:
+            return None
+
+        return Intersect(distance = intersect.distance,
+                         point = intersect.point,
+                         normal = intersect.normal,
+                         sceneObject = self)
 
 
 
